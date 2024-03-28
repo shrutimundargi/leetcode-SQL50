15070994be3e7f8888eaefc1eae2fd5d162a5876
Shruti Mundargi <48567754+shrutimundargi@users.noreply.github.com>
2024-03-28 09:57:30 -0400

    Update solution.sql

--- SUBQUERIES/602. Friend Requests II: Who Has the Most Friends/solution.sql
+++ SUBQUERIES/602. Friend Requests II: Who Has the Most Friends/solution.sql
@@ -1,1 +1,19 @@
 -- Write your MySQL query statement below
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+) AS temp
+GROUP BY id
+HAVING num = (
+    SELECT COUNT(*) AS max_num
+    FROM (
+        SELECT requester_id AS id FROM RequestAccepted
+        UNION ALL
+        SELECT accepter_id AS id FROM RequestAccepted
+    ) AS temp
+    GROUP BY id
+    ORDER BY max_num DESC
+    LIMIT 1
+);
